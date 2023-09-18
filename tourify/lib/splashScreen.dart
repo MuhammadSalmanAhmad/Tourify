7569624eab6e284c13f449e814fd195e09a84968
Muhammad Salman Ahmad <salman.ahmad1@arbisoft.com>
2023-09-18 17:35:06 +0500

    added user authentication states in splash screen

--- tourify/lib/splashScreen.dart
+++ tourify/lib/splashScreen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:tourify/MainPanel/MainPanel.dart';
 import 'package:tourify/Onboarding%20Sceens/onboarding_Screen.dart';
+import 'package:tourify/user_Auth_login_screens/SignIn.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,12 +12,29 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final auth = FirebaseAuth.instance;
   @override
   void initState() {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
     //set timer for splash screen
-    Future.delayed(const Duration(seconds: 5)).then((value) =>
+
+    auth.authStateChanges().listen((User?user) {
+      if (user == null) {
+        Future.delayed(const Duration(seconds: 5)).then((value) =>
+        Navigator.of(context)
+            .pushReplacement(MaterialPageRoute(builder: ((context) {
+          return const SignInScreen();
+        }))));
+      } else {
+       Navigator.of(context)
+            .pushReplacement(MaterialPageRoute(builder: ((context) {
+          return const MainScreen();
+        })));
+      }
+    });
+     Future.delayed(const Duration(seconds: 5)).then((value) =>
         Navigator.of(context)
             .pushReplacement(MaterialPageRoute(builder: ((context) {
           return const OnboardingScreen();
