c257aaa86ade6490fe71e79317e1ebc09962c4b8
Muhammad Salman Ahmad <salman.ahmad1@arbisoft.com>
2023-09-20 10:08:40 +0500

    added drawer, company auth and login screens

--- tourify/lib/MainPanel/MainPanel.dart
+++ tourify/lib/MainPanel/MainPanel.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tourify/MainPanel/Drawer.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tourify"),
+        centerTitle: true,
+        backgroundColor: const Color(0xff1034A6),
+      ),
       drawer: MainPanelDrawer(),
       body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
         Stack(
@@ -14,7 +31,7 @@ class MainScreen extends StatelessWidget {
           alignment: AlignmentDirectional.bottomCenter,
           children: [
             Container(
-              height: MediaQuery.of(context).size.height * 0.4,
+              height: MediaQuery.of(context).size.height * 0.2,
               width: MediaQuery.of(context).size.width,
               decoration: const BoxDecoration(
                   color: Color(0xff1034A6),
@@ -22,16 +39,12 @@ class MainScreen extends StatelessWidget {
                       bottomLeft: Radius.circular(50.0),
                       bottomRight: Radius.circular(50.0))),
             ),
-            Positioned(
-              top: 200,
-              child: Container()
-            ),
+            Positioned(top: 200, child: Container()),
           ],
         ),
         const SizedBox(
           height: 200,
         ),
-       
       ]),
     );
   }
--- tourify/lib/main.dart
+++ tourify/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tourify/Company_Login_Authentication/company_login.dart';
 import 'package:tourify/HomeScreen.dart';
 import 'package:tourify/MainPanel/MainPanel.dart';
 import 'package:tourify/user_Auth_login_screens/SignIn.dart';
 import 'package:tourify/user_Auth_login_screens/SignUp.dart';
 import 'package:tourify/splashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:tourify/Company_Login_Authentication/registeration.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +24,9 @@ void main() async {
       'HomeScreen':(context) => const HomeScreen (),
       'SignUpScreen':(context) => const SignUpScreen (),
       'SignInScreen':(context) => const SignInScreen (),
-      'MainScreen':(context) => const MainScreen (),  
+      'MainScreen':(context) => const MainScreen (), 
+      'CompanyRegistration':(context) => const CompanyRegisteration (),
+      'CompanySignInScreen':(context) => const CompanySignIn (),
 
     },
   ));
